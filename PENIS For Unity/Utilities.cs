5fc26af1b42d026acdccb050b0dc31372a314f6f
Jimmy Cushnie <jimmycushnie@gmail.com>
2018-11-08 16:41:15 -0500

    improved setting Utilities.DefaultPath

--- PENIS For Unity/Utilities.cs
+++ PENIS For Unity/Utilities.cs
@@ -7,7 +7,7 @@ namespace SUCC
 {
     public static class Utilities
     {
-        private static string _DefaultPath;
+        private static string _DefaultPath = null;
 
         /// <summary>
         /// For the built game, this refers to the same folder that the executable is in. In the editor, it refers to a "GameData" folder in the same directory as the Assets folder. You can change it if you like.
@@ -16,7 +16,7 @@ namespace SUCC
         {
             get
             {
-                if (Path.IsPathRooted(_DefaultPath)) { return _DefaultPath; }
+                if (_DefaultPath != null) { return _DefaultPath; }
 #if UNITY_EDITOR
                 string ProjectFolder = Directory.GetParent(Application.dataPath).FullName;
                 return Path.Combine(ProjectFolder, "Game");
