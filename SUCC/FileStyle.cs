5bb66d2fd33656c6a52127916c1c2d0873515387
Jimmy Cushnie <jimmycushnie@gmail.com>
2019-01-17 23:24:55 -0500

    better idea: let's just not use structs
    I hope FileStyle being nullable doesn't come back to haunt me later. C# 8 and non-nullable reference types, I am ready for you.

--- SUCC/FileStyle.cs
+++ SUCC/FileStyle.cs
@@ -8,12 +8,12 @@ namespace SUCC
     /// <remarks>
     /// This only affects new data. If SUCC is modifying existing data, it will do its best to keep the formatting intact.
     /// </remarks>
-    public struct FileStyle
+    public class FileStyle
     {
         /// <summary>
         /// If you do not specify a FileStyle when creating a DataFile, this will be used.
         /// </summary>
-        public static FileStyle Default;
+        public static FileStyle Default = new FileStyle();
 
 
         /// <summary>
@@ -40,12 +40,12 @@ namespace SUCC
         /// <summary>
         /// SUCC strings can optionally be surrounded by "quotes". If this is true, they will be quoted even when not necessary.
         /// </summary>
-        public bool AlwaysQuoteStrings;
+        public bool AlwaysQuoteStrings { get; set; }
 
         /// <summary>
         /// SUCC can store dictionaries as KeyValuePair arrays if the key type is complex. If this is true, dictionaries will always be stored like that.
         /// </summary>
-        public bool AlwaysArrayDictionaries;
+        public bool AlwaysArrayDictionaries { get; set; }
 
         int _IndentationInterval;
         int _SpacesAfterColon;
@@ -56,11 +56,7 @@ namespace SUCC
         /// </summary>
         public int IndentationInterval
         {
-            get
-            {
-                if (_IndentationInterval < 1) _IndentationInterval = 1;
-                return _IndentationInterval;
-            }
+            get => _IndentationInterval;
             set
             {
                 if (value < 1)
