5cd00547d0421f1bfc2343184acf50422dd35ec3
Jimmy Cushnie <jimmycushnie@gmail.com>
2019-01-17 20:21:49 -0500

    FileStyle is now a struct

--- SUCC/FileStyle.cs
+++ SUCC/FileStyle.cs
@@ -3,42 +3,53 @@
 namespace SUCC
 {
     /// <summary>
-    /// Allows you to customize various aspects of how generated SUCC data is formatted.
+    /// Rules for various aspects of how generated SUCC data is formatted.
     /// </summary>
     /// <remarks>
     /// This only affects new data. If SUCC is modifying existing data, it will do its best to keep the formatting intact.
     /// </remarks>
-    public class FileStyle
+    public struct FileStyle
     {
         /// <summary>
         /// If you do not specify a FileStyle when creating a DataFile, this will be used.
         /// </summary>
         public static FileStyle Default { get; set; } = new FileStyle();
 
+
+        /// <summary>
+        /// Creatse a new Style.
+        /// </summary>
+        /// <param name="indentationInterval">The number of spaces used to indent a child line under its parent.</param>
+        /// <param name="spacesAfterColon">The number of spaces between the colon and the value in a key node.</param>
+        /// <param name="spacesAfterDash">The number of spaces between the dash and the value in a list node.</param>
+        /// <param name="alwaysQuoteStrings">SUCC strings can optionally be surrounded by "quotes". If this is true, they will be quoted even when not necessary.</param>
+        /// <param name="alwaysArrayDictionaries">SUCC can store dictionaries as KeyValuePair arrays if the key type is complex. If this is true, dictionaries will always be stored like that.</param>
         public FileStyle(
             int indentationInterval = 4, int spacesAfterColon = 1, int spacesAfterDash = 1, 
             bool alwaysQuoteStrings = true, bool alwaysArrayDictionaries = false)
         {
-            IndentationInterval = indentationInterval;
-            SpacesAfterColon = spacesAfterColon;
-            SpacesAfterDash = spacesAfterDash;
+            _IndentationInterval = indentationInterval;
+            _SpacesAfterColon = spacesAfterColon;
+            _SpacesAfterDash = spacesAfterDash;
             AlwaysQuoteStrings = alwaysQuoteStrings;
             AlwaysArrayDictionaries = alwaysArrayDictionaries;
         }
 
-        int _IndentationInterval = 4;
-        int _SpacesAfterColon = 1;
-        int _SpacesAfterDash = 1;
+
 
         /// <summary>
         /// SUCC strings can optionally be surrounded by "quotes". If this is true, they will be quoted even when not necessary.
         /// </summary>
-        public bool AlwaysQuoteStrings { get; set; } = true;
+        public bool AlwaysQuoteStrings;
 
         /// <summary>
         /// SUCC can store dictionaries as KeyValuePair arrays if the key type is complex. If this is true, dictionaries will always be stored like that.
         /// </summary>
-        public bool AlwaysArrayDictionaries { get; set; } = false;
+        public bool AlwaysArrayDictionaries;
+
+        int _IndentationInterval;
+        int _SpacesAfterColon;
+        int _SpacesAfterDash;
 
         /// <summary>
         /// The number of spaces used to indent a child line under its parent.
