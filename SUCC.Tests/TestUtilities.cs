736eebf887bd35b8456b6e04a09bcd37bc5ecaee
Jimmy Cushnie <jimmycushnie@gmail.com>
2019-03-10 11:44:01 -0400

    added string test

---
+++ SUCC.Tests/TestUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using SUCC;
+
+namespace SUCC.Tests
+{
+    public static class TestUtilities
+    {
+        /// <summary> Whether or not to save the files used in testing to disk </summary>
+        public static bool SaveFiles = true;
+
+        public static string[] GenerateSUCCKeys(int count)
+        {
+            var array = new string[count];
+
+            for (int i = 0; i < count; i++)
+                array[i] = i.ToString();
+
+            return array;
+        }
+    }
+}
