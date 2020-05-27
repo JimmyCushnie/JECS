4f5e2ad987f8e2a11c9b1e6e1f920b9d88606b90
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-05-27 05:56:09 -0400

    added tests for System.Type

---
+++ SUCC.Tests/SaveLoad tests/Base Types/SaveLoad_TypeTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace SUCC.Tests
+{
+    [TestClass]
+    public class SaveLoad_TypeTests
+    {
+        [TestMethod]
+        [DataRow(typeof(int), DisplayName = "Int")]
+        [DataRow(typeof(string), DisplayName = "String")]
+        [DataRow(typeof(int[]), DisplayName = "Int array")]
+        [DataRow(typeof(List<int>), DisplayName = "List of ints")]
+        [DataRow(typeof(List<int[]>), DisplayName = "List of array of ints")]
+        [DataRow(typeof(List<Dictionary<string, int>>), DisplayName = "List of dictionary")]
+        public void SaveLoad_Type(System.Type SAVED_VALUE)
+            => TestUtilities.PerformSaveLoadTest(SAVED_VALUE);
+    }
+}
