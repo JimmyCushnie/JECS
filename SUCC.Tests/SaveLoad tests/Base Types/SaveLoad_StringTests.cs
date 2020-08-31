40b99061eb540175dc780ac7d2810a5ba93fc09b
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-08-31 00:55:58 -0400

    added some tests to ensure the new multi-line string syntax is handled correctly

--- SUCC.Tests/SaveLoad tests/Base Types/SaveLoad_StringTests.cs
+++ SUCC.Tests/SaveLoad tests/Base Types/SaveLoad_StringTests.cs
@@ -19,6 +19,7 @@ namespace SUCC.Tests
         [DataRow("\"", DisplayName = "double quotes")]
         [DataRow("\"\"", DisplayName = "double quotes x2")]
         [DataRow("\"\"\"", DisplayName = "double quotes x3")]
+        [DataRow(@"\", DisplayName = "backslash")]
         [DataRow("#", DisplayName = "pound sign")]
         [DataRow("##", DisplayName = "pound sign x2")]
         [DataRow("###", DisplayName = "pound sign x3")]
@@ -28,6 +29,7 @@ namespace SUCC.Tests
         [DataRow(multiLineStringWithPoundSigns, DisplayName = "multi line string with pound signs")]
         [DataRow(multiLineStringWithDoubleQuotes, DisplayName = "multi line string with double quotes")]
         [DataRow(multiLineStringWithLeadingTrailingSpaces, DisplayName = "multi line string with leading and trailing spaces")]
+        [DataRow(multiLineStringWithBackslashes, DisplayName = "multi line string with backslashes")]
         [DataRow(veryLongString, DisplayName = "very long string")]
         [DataRow(poem, DisplayName = "poem")]
         [DataRow(genemoji, DisplayName = "genemoji")]
@@ -73,6 +75,18 @@ string with multiple lines
    and leading spaces
 and trailing spaces    
     and both!    
+";
+
+        const string multiLineStringWithBackslashes = @"
+
+\
+
+\\
+
+\\\\\
+
+""\""
+
 ";
 
         const string veryLongString = "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my public class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.";
