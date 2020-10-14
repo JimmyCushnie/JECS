1eaca29182b9d3689c2d974ff43bb884ea6d11bc
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-10-14 01:25:03 -0400

    added IPAddress as a built-in base type

---
+++ SUCC.Tests/SaveLoad tests/Base Types/SaveLoad_IPAddressTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace SUCC.Tests
+{
+    [TestClass]
+    public class SaveLoad_IPAddressTests
+    {
+        [TestMethod]
+        public void SaveLoad_IP()
+        {
+            string hostName = Dns.GetHostName();
+            IPAddress ip = Dns.GetHostAddresses(hostName)[0];
+            TestUtilities.PerformSaveLoadTest(ip);
+        }
+    }
+}
