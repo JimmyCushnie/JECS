9cd52c3e12384ab5b84bf3f74d6043e1d2b078db
Jimmy Cushnie <jimmy@mousehatgames.com>
2022-02-02 00:22:53 -0500

    Throw exception when a base type fails to parse; create tests for it

---
+++ SUCC.Tests/Parsing error tests/InvalidBaseTypeNodeDataTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SUCC.MemoryFiles;
+
+namespace SUCC.Tests
+{
+    [TestClass]
+    public class InvalidBaseTypeNodeDataTests
+    {
+        [TestMethod]
+        public void InvalidFileData_InvalidBaseType_InvalidNodeValue()
+        {
+            var file = new MemoryReadOnlyDataFile(ValidFileStructure_InvalidIntData);
+
+            Assert.ThrowsException<CannotRetrieveDataFromNodeException>(() =>
+            {
+                file.Get<int>("IntValue");
+            });
+        }
+
+        [TestMethod]
+        public void InvalidFileData_InvalidBaseType_BadSpecialStringCase()
+        {
+            var file = new MemoryReadOnlyDataFile(ValidFileStructure_InvalidIntData);
+
+            Assert.ThrowsException<CannotRetrieveDataFromNodeException>(() =>
+            {
+                file.Get<int>("IntValue2");
+            });
+        }
+
+        const string ValidFileStructure_InvalidIntData = @"
+
+IntValue: notanint
+
+IntValue2: """"""
+    lmao it's a string
+    """"""
+
+# this file also has a comment because why not
+
+";
+    }
+}
--- SUCC/Parsing Logic/NodeManager.cs
+++ SUCC/Parsing Logic/NodeManager.cs
@@ -59,27 +59,29 @@ namespace SUCC.ParsingLogic
             // this ensures the base type rules are registered before they are needed.
             System.Runtime.CompilerServices.RuntimeHelpers.RunClassConstructor(type.TypeHandle);
 
-            try
-            {
-                if (type == typeof(string) && node.Value == MultiLineStringNode.Terminator && node.ChildLines.Count > 0)
-                    return MultiLineStringSpecialCaseHandler.ParseSpecialStringCase(node);
+            if (type == typeof(string) && node.Value == MultiLineStringNode.Terminator && node.ChildLines.Count > 0)
+                return MultiLineStringSpecialCaseHandler.ParseSpecialStringCase(node);
 
-                if (BaseTypesManager.IsBaseType(type))
+            if (BaseTypesManager.IsBaseType(type))
+            {
+                try
+                {
                     return RetrieveBaseTypeNode(node, type);
-
-                var collection = CollectionTypes.TryGetCollection(node, type);
-                if (collection != null) 
-                    return collection;
-
-                if (!node.Value.IsNullOrEmpty())
-                    return ComplexTypeShortcuts.GetFromShortcut(node.Value, type);
-
-                return ComplexTypes.RetrieveComplexType(node, type);
+                }
+                catch
+                {
+                    throw new CannotRetrieveDataFromNodeException(node, type);
+                }
             }
-            catch (Exception e)
-            {
-                throw new Exception($"Error getting data of type {type} from node", e);
-            }
+
+            var collection = CollectionTypes.TryGetCollection(node, type);
+            if (collection != null) 
+                return collection;
+
+            if (!node.Value.IsNullOrEmpty())
+                return ComplexTypeShortcuts.GetFromShortcut(node.Value, type);
+
+            return ComplexTypes.RetrieveComplexType(node, type);
         }
 
 
@@ -96,7 +98,7 @@ namespace SUCC.ParsingLogic
             // it will still work.
             // See https://github.com/JimmyCushnie/SUCC/issues/26
 
-            if (node.ChildNodes.Count > 0)
+            if (node.ChildNodeType == NodeChildrenType.key && node.ChildNodes.Count > 0)
                 return ComplexTypes.RetrieveComplexType(node, type);
 
             if (BaseTypesManager.TryParseBaseType(node.Value, type, out var result))
