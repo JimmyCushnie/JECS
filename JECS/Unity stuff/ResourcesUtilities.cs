583ec00776c86305246102d671adb6528b53f4f4
Jimmy Cushnie <jimmy@mousehatgames.com>
2024-04-28 00:53:39 -0400

    Add DataFile.WithDefaultFile and ReadOnlyDataFile.WithDefaultFile

---
+++ JECS/Unity stuff/ResourcesUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace JECS.UnityStuff
+{
+    public static class ResourcesUtilities
+    {
+        public static string ReadTextFromFile(string filePath)
+        {
+            var textAsset = Resources.Load<TextAsset>(filePath);
+            if (textAsset == null)
+                throw new FileNotFoundException("The default file you specified doesn't exist in Resources :(");
+
+            var text = textAsset.text;
+            Resources.UnloadAsset(textAsset);
+
+            return text;
+        }
+    }
+}
