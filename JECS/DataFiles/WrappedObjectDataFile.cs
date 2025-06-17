505e879d5c028606b083012209b49d0df6376be7
Ecconia <public-commit@ecconia.com>
2025-06-18 00:31:04 +0200

    Add WrappedObjectDataFile
    This file is moved from a project using JECS into JECS. It was originally written by the JECS author Jimmy.
    
    There are many applications where a single object is stored as a single JECS file. This new class makes it easy to load this data and work with it - reducing code overhead.
    
    Due to it's structure 'Save()' has to be called explicitly to sync the data back into the JECS file. But unless working with JECS explicitly this has to be done anyway.

---
+++ JECS/DataFiles/WrappedObjectDataFile.cs
@@ -0,0 +1,34 @@
+﻿namespace JECS
+{
+    /// <summary>
+    /// Use when you have some data (of type <typeparamref name="T"/>) that's represented by a file on disk.
+    /// When the <see cref="WrappedObjectDataFile{T}"/> is created, <see cref="Data"/> is loaded from the file.
+    /// If you change <see cref="Data"/>, call <see cref="Save"/> to save your changes to disk.
+    /// </summary>
+    public class WrappedObjectDataFile<T>
+    {
+        private readonly DataFile DiskFile;
+        public readonly T Data;
+
+        public WrappedObjectDataFile(string filePath, T defaultValue)
+        {
+            DiskFile = new DataFile(filePath);
+            DiskFile.AutoSave = false;
+
+            Data = DiskFile.GetAsObject(defaultValue);
+        }
+
+        public WrappedObjectDataFile(string filePath, string defaultFileText = null)
+        {
+            DiskFile = new DataFile(filePath, defaultFileText);
+            DiskFile.AutoSave = false;
+            Data = DiskFile.GetAsObject<T>();
+        }
+
+        public void Save()
+        {
+            DiskFile.SaveAsObject(Data);
+            DiskFile.SaveAllData();
+        }
+    }
+}
